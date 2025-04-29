b485ff46f2fff69ea5a4b78381cd153df12e18f9
Lucas Ferreira Lourenço <141689321+LucasFL05@users.noreply.github.com>
2025-04-29 15:59:27 -0300

    Remoção de Instances desnecessárias

--- src/main/java/com/cltech/assistencia_tecnica/mapper/ClienteMapper.java
+++ src/main/java/com/cltech/assistencia_tecnica/mapper/ClienteMapper.java
@@ -3,12 +3,9 @@ package com.cltech.assistencia_tecnica.mapper;
 import com.cltech.assistencia_tecnica.dto.ClienteDTO;
 import com.cltech.assistencia_tecnica.model.Cliente;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface ClienteMapper {
-
-    ClienteMapper INSTANCE = Mappers.getMapper(ClienteMapper.class);
 
     ClienteDTO clienteToClienteDTO(Cliente cliente);
 
