7bc9fcac0dc6e1081cfdf7120a48e72dea036bf1
Lucas Ferreira Lourenço <141689321+LucasFL05@users.noreply.github.com>
2025-05-20 16:20:29 -0300

    Create RelatorioOperacionalService.java

---
+++ src/main/java/com/cltech/assistencia_tecnica/service/RelatorioOperacionalService.java
@@ -0,0 +1,7 @@
+package com.cltech.assistencia_tecnica.service;
+
+import com.cltech.assistencia_tecnica.dto.RelatorioOperacionalDTO;
+
+public interface RelatorioOperacionalService {
+    RelatorioOperacionalDTO gerarRelatorio();
+}
